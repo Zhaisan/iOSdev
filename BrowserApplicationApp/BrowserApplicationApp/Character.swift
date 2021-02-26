74d3f7b001d5a975a7da77372685fed511541eca
Zhaisan Sarsengaliyev <zhaisansars@gmail.com>
2021-02-26 17:38:16 +0600

    iOS

---
+++ BrowserApplicationApp/BrowserApplicationApp/Character.swift
@@ -0,0 +1,18 @@
+//
+//  Character.swift
+//  BrowserApplicationApp
+//
+//  Created by Zhaisan on 25.02.2021.
+//
+
+import Foundation
+import UIKit
+
+struct Character{
+    
+    var name: String?
+    var url: String?
+    var isFavourite = false
+}
+
+
